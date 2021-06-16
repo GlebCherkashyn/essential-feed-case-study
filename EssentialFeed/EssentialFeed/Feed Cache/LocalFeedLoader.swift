c6412a4b5e77ceb66130b83e51a31f03fc08d64f
Gleb <cherka_g@modulotech.fr>
2021-06-16 23:04:46 +0300

    Make the `FeedCachePolicy` the pure type with no side effects (deterministic)

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -9,32 +9,26 @@
 import Foundation
 
 private final class FeedCachePolicy {
-  private let currentDate: () -> Date
   private let calendar = Calendar.current
   private var maxCacheAgeInDays = 7
-  
-  init(currentDate: @escaping () -> Date) {
-    self.currentDate = currentDate
-  }
     
-  func validate(_ timestamp: Date) -> Bool {
+	func validate(_ timestamp: Date, against date: Date) -> Bool {
     guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
       return false
     }
     
-    return currentDate() < maxCacheAge
+    return date < maxCacheAge
   }
 }
 
 public final class LocalFeedLoader {
   private let currentDate: () -> Date
   private let store: FeedStore
-  private let cachePolicy: FeedCachePolicy
+  private let cachePolicy = FeedCachePolicy()
   
   public init(store: FeedStore, currentDate: @escaping () -> Date) {
     self.store = store
     self.currentDate = currentDate
-    self.cachePolicy = FeedCachePolicy(currentDate: currentDate)
   }
 }
 
@@ -70,7 +64,7 @@ extension LocalFeedLoader: FeedLoader {
       case let .failure(error):
         completion(.failure(error))
         
-      case let .found(feed, timestamp) where self.cachePolicy.validate(timestamp):
+			case let .found(feed, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
         completion(.success(feed.toModels()))
         
       case .found, .empty:
@@ -89,7 +83,7 @@ extension LocalFeedLoader {
       case .failure:
         self.store.deleteCachedFeed { _ in }
         
-      case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp):
+			case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
         self.store.deleteCachedFeed { _ in }
         
       case .found, .empty:
