6d6f7b473c7eb51cfa683e8472cfee54f1f426aa
Gleb <cherka_g@modulotech.fr>
2021-06-24 02:00:15 +0300

    Extract sut creation into a factory method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -80,13 +80,8 @@ final class CodableFeedStoreTests: XCTestCase {
 		cleanArtefacts()
 	}
 	
-	private func cleanArtefacts() {
-		let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
-		try? FileManager.default.removeItem(at: storeURL)
-	}
-	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		let exp = expectation(description: "Wait for cache retrieval")
 		
 		sut.retrieve { result in
@@ -105,7 +100,7 @@ final class CodableFeedStoreTests: XCTestCase {
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		let exp = expectation(description: "Wait for cache retrieval")
 		
 		sut.retrieve { firstResult in
@@ -126,7 +121,7 @@ final class CodableFeedStoreTests: XCTestCase {
 	}
 	
 	func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		let feed = uniqueImageFeed.local
 		let timestamp = Date()
 		let exp = expectation(description: "Wait for cache retrieval")
@@ -149,5 +144,16 @@ final class CodableFeedStoreTests: XCTestCase {
 		}
 		
 		wait(for: [exp], timeout: 1)
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSUT() -> CodableFeedStore {
+		CodableFeedStore()
+	}
+	
+	private func cleanArtefacts() {
+		let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+		try? FileManager.default.removeItem(at: storeURL)
 	}
 }
