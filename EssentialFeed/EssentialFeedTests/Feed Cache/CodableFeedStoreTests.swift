891ed7b459ace4d0e5918f7299e2152664d4d9e4
Gleb <cherka_g@modulotech.fr>
2021-06-25 22:54:23 +0300

    Extract duplicate insert code into a helper function

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -101,12 +101,7 @@ final class CodableFeedStoreTests: XCTestCase {
 		let feed = uniqueImageFeed.local
 		let timestamp = Date()
 		
-		let exp = expectation(description: "Wait for cache retrieval")
-		sut.insert(feed, timestamp: timestamp) { insertionError in
-			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 1)
+		insert((feed, timestamp), to: sut)
 		
 		expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
 	}
@@ -116,13 +111,7 @@ final class CodableFeedStoreTests: XCTestCase {
 		let feed = uniqueImageFeed.local
 		let timestamp = Date()
 		
-		let exp = expectation(description: "Wait for cache insertion")
-		sut.insert(feed, timestamp: timestamp) { insertionError in
-			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-			exp.fulfill()
-		}
-		
-		wait(for: [exp], timeout: 1)
+		insert((feed, timestamp), to: sut)
 		
 		expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
 	}
@@ -134,6 +123,15 @@ final class CodableFeedStoreTests: XCTestCase {
 		trackForMemoryLeaks(sut)
 		
 		return sut
+	}
+	
+	private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+		let exp = expectation(description: "Wait for cache insertion")
+		sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
+			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1)
 	}
 	
 	private func expect(_ sut: CodableFeedStore,
@@ -148,7 +146,7 @@ final class CodableFeedStoreTests: XCTestCase {
 											toRetrieve expectedResult: RetrieveCacheFeedResult,
 											file: StaticString = #file,
 											line: UInt = #line) {
-		let exp = expectation(description: "Wait for cache retieval")
+		let exp = expectation(description: "Wait for cache retrieval")
 		
 		sut.retrieve { retrievedResult in
 			switch (expectedResult, retrievedResult) {
