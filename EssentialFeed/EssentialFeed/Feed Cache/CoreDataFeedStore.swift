44bc6848eb18b721bb49319c40aedcf1f68a90f6
Gleb <cherka_g@modulotech.fr>
2021-07-22 00:20:26 +0300

    Extract model translations into helper methods within the managed models

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -13,8 +13,9 @@ public final class CoreDataFeedStore: FeedStore {
 	private let container: NSPersistentContainer
 	private let context: NSManagedObjectContext
 
-	public init(bundle: Bundle = .main) throws {
-		container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+	public init(storeURL: URL,
+							bundle: Bundle = .main) throws {
+		container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
 		context = container.newBackgroundContext()
 	}
 
@@ -25,13 +26,7 @@ public final class CoreDataFeedStore: FeedStore {
 				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
 				request.returnsObjectsAsFaults = false
 				if let cache = try context.fetch(request).first {
-					completion(.found(
-											feed: cache.feed
-												.compactMap { ($0 as? ManagedFeedImage) }
-												.map {
-													LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
-												},
-											timestamp: cache.timestamp))
+					completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
 				} else {
 					completion(.empty)
 				}
@@ -47,14 +42,7 @@ public final class CoreDataFeedStore: FeedStore {
 			do {
 				let managedCache = ManagedCache(context: context)
 				managedCache.timestamp = timestamp
-				managedCache.feed = NSOrderedSet(array: feed.map { local in
-					let managed = ManagedFeedImage(context: context)
-					managed.id = local.id
-					managed.imageDescription = local.description
-					managed.location = local.location
-					managed.url = local.url
-					return managed
-				})
+				managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
 				
 				try context.save()
 				completion(nil)
@@ -73,6 +61,10 @@ public final class CoreDataFeedStore: FeedStore {
 private class ManagedCache: NSManagedObject {
 	@NSManaged var timestamp: Date
 	@NSManaged var feed: NSOrderedSet
+	
+	var localFeed: [LocalFeedImage] {
+		feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+	}
 }
 
 @objc(ManagedFeedImage)
@@ -82,6 +74,22 @@ private class ManagedFeedImage: NSManagedObject {
 	@NSManaged var location: String?
 	@NSManaged var url: URL
 	@NSManaged var cache: ManagedCache
+	
+	static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: localFeed.map { local in
+			let managed = ManagedFeedImage(context: context)
+			managed.id = local.id
+			managed.imageDescription = local.description
+			managed.location = local.location
+			managed.url = local.url
+			
+			return managed
+		})
+	}
+	
+	var local: LocalFeedImage {
+		LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+	}
 }
 
 private extension NSPersistentContainer {
@@ -90,12 +98,18 @@ private extension NSPersistentContainer {
 		case failedToLoadPersistentStores(Swift.Error)
 	}
 
-	static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+	static func load(modelName name: String,
+									 url: URL,
+									 in bundle: Bundle) throws -> NSPersistentContainer {
 		guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
 			throw LoadingError.modelNotFound
 		}
-
+		
+		let description = NSPersistentStoreDescription(url: url)
 		let container = NSPersistentContainer(name: name, managedObjectModel: model)
+		
+		container.persistentStoreDescriptions = [description]
+		
 		var loadError: Swift.Error?
 		container.loadPersistentStores { loadError = $1 }
 		try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
